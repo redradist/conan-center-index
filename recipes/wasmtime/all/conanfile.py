0f6b58f696057bdc4395519831a3adbaccfd2c60
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-08-09 14:13:50 +0200

    wasmtime: bump to 0.29.0 + mingw + MSVC

--- recipes/wasmtime/all/conanfile.py
+++ recipes/wasmtime/all/conanfile.py
@@ -5,6 +5,7 @@ import shutil
 
 required_conan_version = ">=1.33.0"
 
+
 class WasmtimeConan(ConanFile):
     name = 'wasmtime'
     homepage = 'https://github.com/bytecodealliance/wasmtime'
@@ -21,7 +22,10 @@ class WasmtimeConan(ConanFile):
         'shared': False,
         'fPIC': True,
     }
-    no_copy_source = True
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
     @property
     def _minimum_cpp_standard(self):
@@ -36,15 +40,26 @@ class WasmtimeConan(ConanFile):
             "gcc": "4.9.4"
         }
 
+    @property
+    def _sources_key(self):
+        if self.settings.compiler == "Visual Studio":
+            return "Visual Studio"
+        elif self.settings.os == "Windows" and self.settings.compiler == "gcc":
+            return "mingw"
+        return {
+            "Android": "Linux",
+        }.get(str(self.settings.os), str(self.settings.os))
+
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
     def configure(self):
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
         if self.options.shared:
             del self.options.fPIC
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+        del self.settings.compiler.runtime
 
     def validate(self):
         compiler = self.settings.compiler
@@ -63,37 +78,48 @@ class WasmtimeConan(ConanFile):
             self.output.warn(msg)
 
         try:
-            self.conan_data["sources"][self.version][str(self.settings.os)]
+            self.conan_data["sources"][self.version][self._sources_key]
         except KeyError:
             raise ConanInvalidConfiguration("Binaries for this combination of architecture/version/os not available")
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version][str(self.settings.os)][str(self.settings.arch)], destination=self.source_folder, strip_root=True)
+        if (self.settings.compiler, self.settings.os) == ("gcc", "Windows") and self.options.shared:
+            # https://github.com/bytecodealliance/wasmtime/issues/3168
+            raise ConanInvalidConfiguration("Shared mingw is currently not possible")
+
+    def build(self):
+        tools.get(**self.conan_data["sources"][self.version][self._sources_key][str(self.settings.arch)],
+                  destination=self._source_subfolder, strip_root=True)
 
     def package(self):
-        shutil.copytree(os.path.join(self.source_folder, "include"),
+        self.copy('LICENSE', src=self._source_subfolder, dst='licenses')
+        shutil.copytree(os.path.join(self._source_subfolder, "include"),
                         os.path.join(self.package_folder, "include"))
 
-        self.copy('*.lib', dst='lib', keep_path=False)
-        self.copy('*.dll', dst='bin', keep_path=False)
-        self.copy('*.so', dst='lib', keep_path=False)
-        self.copy('*.dylib', dst='lib', keep_path=False)
-        self.copy('*.a', dst='lib', keep_path=False)
-
-        self.copy('LICENSE', dst='licenses', src=self.source_folder)
+        srclibdir = os.path.join(self._source_subfolder, "lib")
+        if self.options.shared:
+            self.copy('wasmtime.dll.lib', src=srclibdir, dst='lib', keep_path=False)
+            self.copy('wasmtime.dll', src=srclibdir, dst='bin', keep_path=False)
+            self.copy('libwasmtime.dll.a', src=srclibdir, dst='lib', keep_path=False)
+            self.copy('libwasmtime.so*', src=srclibdir, dst='lib', keep_path=False)
+            self.copy('libwasmtime.dylib', src=srclibdir,  dst='lib', keep_path=False)
+        else:
+            self.copy('wasmtime.lib', src=srclibdir, dst='lib', keep_path=False)
+            self.copy('libwasmtime.a', src=srclibdir, dst='lib', keep_path=False)
 
     def package_info(self):
-        if self.options.shared:
-            if self.settings.os == "Windows":
-                self.cpp_info.libs = ["wasmtime.dll"]
+        if self.settings.os == "Windows":
+            static_suffix = ".lib" if self.settings.compiler == "Visual Studio" else ".a"
+            if self.options.shared:
+                libsuffix = ".dll{}".format(static_suffix)
             else:
-                self.cpp_info.libs = ["wasmtime"]
+                libsuffix = static_suffix
+                self.cpp_info.defines = ["WASM_API_EXTERN=", "WASI_API_EXTERN="]
         else:
-            if self.settings.os == "Windows":
-                self.cpp_info.defines= ["/DWASM_API_EXTERN=", "/DWASI_API_EXTERN="]
-            self.cpp_info.libs = ["wasmtime"]
+            libsuffix = ""
+        self.cpp_info.libs = ["wasmtime" + libsuffix]
 
-        if self.settings.os == 'Windows':
-            self.cpp_info.system_libs = ['ws2_32', 'bcrypt', 'advapi32', 'userenv', 'ntdll', 'shell32', 'ole32']
-        elif self.settings.os == 'Linux':
-            self.cpp_info.system_libs = ['pthread', 'dl', 'm']
+        if not self.options.shared:
+            if self.settings.os == 'Linux':
+                self.cpp_info.system_libs = ['pthread', 'dl', 'm']
+            elif self.settings.os == "Windows":
+                self.cpp_info.system_libs = ['ws2_32', 'bcrypt', 'advapi32', 'userenv', 'ntdll', 'shell32', 'ole32']
